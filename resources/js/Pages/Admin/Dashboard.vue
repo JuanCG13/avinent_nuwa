fc529e42a27c438fbac544ec6a083d0e0af3a279
Quim <quim.alguero@nuwa.cat>
2023-07-01 01:39:43 +0200

    DataTable styles

--- resources/js/Pages/Admin/Dashboard.vue
+++ resources/js/Pages/Admin/Dashboard.vue
@@ -8,6 +8,9 @@ import Row from 'primevue/row';
 import Message from 'primevue/message';      
 import Breadcrumb from 'primevue/breadcrumb';
 import moment from 'moment';
+import { useToast } from 'primevue/usetoast';
+
+const toast = useToast();
 
 const _data = defineProps({
 
@@ -34,8 +37,12 @@ const _status = ((idEstat)=> {
 
 });
 
+const showSuccess = () => {
+};
+
 
 onMounted(() => {
+    toast.add({ severity: 'success', summary: 'Success Message', detail: 'Message Content', life: 3000 });
 //   CustomerService.getCustomersMedium().then((data) => (customers.value = data));
 });
 
@@ -50,26 +57,46 @@ onMounted(() => {
         </template>
 
         <template #content>
-            <div class="py-12 lg:columns-2 gap-8">
-                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    
-                    <Message severity="info" icon="pi pi-send">Info Message Content</Message>
-                    
+            <div class="py-12 lg:columns-2 gap-0">
+                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">         
+                       <Message severity="success">
+                        <div class="flex flex-col">
+                            <span class="font-bold">¡Tienes clientes pendientes de validación! </span>
+                            <span class="text-xs">Success Message Content</span>
+                        </div>
+                    </Message>
+                    <Message severity="info">Info Message Content</Message>
+                    <Message severity="warn">Warning Message Content</Message>
+                    <Message severity="error">Error Message Content</Message>
+                
                     <DataTable :value="_data.users" paginator :rows="10" :rowsPerPageOptions="[10, 20, 50]">
-                        <Column header="Data" style="width: 25%">
+                      
+                     <template #header>
+                        <div class="flex flex-wrap align-items-center">
+                            <span class="text-lg bg-gray-200 dark:bg-gray-900 font-bold">Validación de clientes</span>
+                        </div>
+                    </template>
+                        <Column header="Data" >
                             <template #body="{data}">
                                 {{ moment(data.created_at).format('DD/MM/YYYY') }}
                             </template>
                        
                         </Column>
-                        <Column field="raoSaocial" header="Raó social" style="width: 25%"></Column>
-                        <Column field="estat" header="Estat" style="width: 25%">
+                        <Column field="raoSocial" header="Raó social" ></Column>
+                        <Column field="estat" header="Estat" >
                             <template #body="{data}">
                                 {{ _status(data.estat) }}
                             </template>
                        
                         </Column>
-                        <Column field="idClientX3" header="ID Client" style="width: 25%"></Column>
+                        <Column field="idClientX3" header="ID Client" ></Column>
+                        <!-- <template #footer> In total there are {{ _data.users ? _data.users.length: 0 }} clients. </template> -->
+                        <Column field="id">
+                            <template #body="{data}">
+                                <i class="pi pi-eye text-primary-500"></i>
+                                <!-- {{ data.id }} -->
+                            </template>
+                        </Column>
                     </DataTable>
 
                 </div>
@@ -80,11 +107,27 @@ onMounted(() => {
 
                     <Message severity="info" icon="pi pi-send">Info Message Content</Message>
 
-                    <DataTable :value="_data.orders" paginator :rows="20" :rowsPerPageOptions="[10, 20, 50]">
-                        <Column field="dataCreacio" header="Data" style="width: 25%"></Column>
-                        <Column field="raoSocial" header="Raó social" style="width: 25%"></Column>
-                        <Column field="estat(idEstat)" header="Estat" style="width: 25%"></Column>
-                        <Column field="idComandaX3" header="ID Cas" style="width: 25%"></Column>
+                    <DataTable :value="_data.users" paginator :rows="20" :rowsPerPageOptions="[10, 20, 50]">
+                    <template #header>
+                        <div class="flex flex-wrap align-items-center">
+                            <span class="text-lg bg-gray-200 dark:bg-gray-900 font-bold">Validación de pedidos</span>
+                        </div>
+                    </template>
+                        <Column header="Data" >
+                            <template #body="{data}">
+                                {{ moment(data.dataCreacio).format('DD/MM/YYYY') }}
+                            </template>
+                        </Column>
+                        <Column field="raoSocial" header="Raó social" ></Column>
+                        <Column field="estat(idEstat)" header="Estat" ></Column>
+                        <Column field="idComandaX3" header="ID Cas" ></Column>
+                        <Column field="id">
+                            <template #body="{data}">
+                                <i class="pi pi-check-circle text-primary-500"></i>
+                                <!-- {{ data.id }} -->
+                            </template>
+                        </Column>
+
                     </DataTable>
 
                 </div>
