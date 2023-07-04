7adec61a4ef7436e81d83d8d3f5764303bc48898
Quim <quim.alguero@nuwa.cat>
2023-07-04 11:06:03 +0200

    Modify components

--- resources/js/Components/SecondaryButton.vue
+++ resources/js/Components/SecondaryButton.vue
@@ -4,14 +4,14 @@ import Button from "primevue/button";
 defineProps({
     type: {
         type: String,
-        default: 'submit',
+        default: 'button',
     },
 });
 </script>
 
 <template>
     <Button
-        :type="submit"
+        :type="type"
         class="flex justify-center rounded-md border border-transparent bg-gray-500 hover:!bg-primary-500 py-2 px-4 text-sm font-medium text-white shadow-sm focus:outline-none focus:ring-2 focus:ring-copate-500 focus:ring-offset-2 transition ease-in-out duration-150"
     >
      <slot />
