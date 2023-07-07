2d633d50a9a89941745d99c1221cf5de2d83add9
Quim <quim.alguero@nuwa.cat>
2023-07-07 20:51:18 +0200

    Update impersonation controller

--- app/Http/Controllers/ImpersonationController.php
+++ app/Http/Controllers/ImpersonationController.php
@@ -6,14 +6,26 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use Inertia\Inertia;
 
 class ImpersonationController extends Controller
     {
     
         public function impersonate(Request $request, User $user)
         {
-            Auth::user()->impersonate($user);
-            //return redirect()->intended('dashboard');
+            
+            if (Auth::user()->canImpersonate()) {
+                $impersionationuser = Auth::User();
+                Auth::user()->impersonate($user);
+
+                // return Inertia::render('dashboard', [
+                //     'impersonationUser' => $impersionationuser, 
+                // ]);
+            }
+
+   
+            //return redirect()->route('welcome');
+
         }
     
         public function leaveImpersonate()
