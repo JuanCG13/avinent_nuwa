831543754cf0538f1037aa8f45acee861e2d95ee
Quim <quim.alguero@nuwa.cat>
2023-07-21 12:06:21 +0200

    Add customer registration validation type A

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -11,6 +11,9 @@ use Laravel\Fortify\Contracts\CreatesNewUsers;
 use Laravel\Jetstream\Jetstream;
 use App\Notifications\UserWelcome;
 
+
+
+
 class CreateNewUser implements CreatesNewUsers
 {
     use PasswordValidationRules;
@@ -22,19 +25,29 @@ class CreateNewUser implements CreatesNewUsers
      */
     public function create(array $input): User
     {
-    
 
         if ($input['customer_type'] == 'A')  {
-            Validator::make($input, [
+
+            $validator = Validator::make($input, [
                 'idClientX3' => ['required', 'string', 'unique:users'],
                 'cif' => ['required', 'string'],
                 'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
                 'password' => $this->passwordRules(),
                 'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
-            ])->validate();
+            ]);
+                  
+            $validator->after(function ($validator) use ($input)  {
+                if ($this->checkX3CIFPair($input) == false) {  
+                    $validator->errors()->add('idClientX3', __('idX3CIF'));
+                }
+    
+            });
 
-            
-            return $this->createUserTypeA($input);
+            $validator->validate();
+
+            $newUser = $this->createUserTypeA($input);
+
+            return $newUser;
         }
 
         if ($input['customer_type'] == 'B')  {
@@ -53,6 +66,19 @@ class CreateNewUser implements CreatesNewUsers
 
     }
 
+    protected function checkX3CIFPair($input): Bool {
+
+        if(DB::table('tClients')
+        ->select('raoSocial')
+        ->whereRaw("idClientX3 = '".$input['idClientX3']."' AND CIF = '".$input['cif']."'")
+        ->first()) {
+            return true;
+        }
+
+        return false;
+        
+    }
+
     protected function createUserTypeA($input): User {
 
         $client = DB::table('tClients')
@@ -60,28 +86,23 @@ class CreateNewUser implements CreatesNewUsers
                     ->whereRaw("idClientX3 = '".$input['idClientX3']."' AND CIF = '".$input['cif']."'")
                     ->first();
                 
-                if ($client) {
-                    return DB::transaction(function () use ($input, $client) {
-                        return tap(
-                            User::create([
-                            'idClientX3' => $input['idClientX3'],
-                            'CIF' => $input['cif'],
-                            'name' => $client->raoSocial,
-                            'email' => $input['email'],
-                            'password' => Hash::make($input['password']),
-                            'estat' => 1,
-                            'tipusUsuari' => 1,
-                            'isAdmin' => 0,
-                            ]
-                    ), function (User $user) {
-                            $this->createTeam($user);
-                        });
-                    });
-            } else {
+        return DB::transaction(function () use ($input, $client) {
+            return tap(
+                User::create([
+                'idClientX3' => $input['idClientX3'],
+                'CIF' => $input['cif'],
+                'name' => $client->raoSocial,
+                'email' => $input['email'],
+                'password' => Hash::make($input['password']),
+                'estat' => 1,
+                'tipusUsuari' => 1,
+                'isAdmin' => 0,
+                ]
+        ), function (User $user) {
+                $this->createTeam($user);
+            });
+        });
 
-                return null;
-                
-            }
 
     }
 
--- resources/js/Pages/Auth/RegisterTypeA.vue
+++ resources/js/Pages/Auth/RegisterTypeA.vue
@@ -1,11 +1,15 @@
 <script setup>
-import { ref } from "vue";
+import { ref, watch } from "vue";
 import { Head, Link, useForm } from "@inertiajs/vue3";
 import Button from "@/Components/PrimaryButton.vue";
 import Checkbox from "@/Components/Checkbox.vue";
 import InputError from "@/Components/InputError.vue";
 import InputLabel from "@/Components/InputLabel.vue";
 import TextInput from "@/Components/TextInput.vue";
+import { useToast } from "primevue/usetoast";
+import Toast from 'primevue/toast';
+
+const toast = useToast();
 
 const form = useForm({
     idClientX3: '',
@@ -22,8 +26,16 @@ const submit = () => {
         onFinish: () => form.reset('password', 'password_confirmation'),
     });
 };
+
+watch(form.hasErrors,() => {
+    alert();
+    toast.add({ severity: 'error', summary: 'Error', detail: 'Debes revisar los campos indicados.', life: 3000 });
+});
+
 </script>
 <template>
+
+    <Toast/>
     <form @submit.prevent="submit">
         <div>
             <InputLabel for="customercode" :value="$t('msg.register-id')" />
